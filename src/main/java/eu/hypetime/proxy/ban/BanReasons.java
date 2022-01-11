e34af43195caca32e46eab5d66e2e9397dcf4297
QuadrixYT <andrehillay@gmail.com>
2022-01-11 19:33:39 +0100

    Create Ban Enum class and add first two reasons

---
+++ src/main/java/eu/hypetime/proxy/ban/BanReasons.java
@@ -0,0 +1,43 @@
+package eu.hypetime.proxy.ban;
+
+/*
+    Created by Andre
+    At 19:25 Uhr | 11. Jan.. 2022
+    Project ProxySystem-v3
+*/
+public enum BanReasons {
+
+     HACKING(1, "Cheating", -1),
+     OTHER(99, "Other", 1);
+
+     private int id;
+     private String name;
+     private long time;
+
+     BanReasons(int id, String name, long time) {
+          this.id = id;
+          this.name = name;
+          this.time = time;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public int getId() {
+          return id;
+     }
+
+     public long getTime() {
+          return time;
+     }
+
+     public static BanReasons getReason(int id) {
+          for (BanReasons reason : BanReasons.values()) {
+               if(reason.getId() == id) {
+                    return reason;
+               }
+          }
+          return BanReasons.HACKING;
+     }
+}
---
+++ src/main/java/eu/hypetime/proxy/lang/LanguageManager.java
@@ -0,0 +1,9 @@
+package eu.hypetime.proxy.lang;
+
+/*
+    Created by Andre
+    At 19:33 Uhr | 11. Jan.. 2022
+    Project ProxySystem-v3
+*/
+public class LanguageManager {
+}
