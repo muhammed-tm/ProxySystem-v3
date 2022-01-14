fe76d98e1151e8939abb79d3908da6bf9a4eb497
QuadrixYT | André <andrehillay@gmail.com>
2022-01-14 15:44:38 +0100

    Format

--- src/main/java/eu/hypetime/proxy/bots/dc/DiscordBot.java
+++ src/main/java/eu/hypetime/proxy/bots/dc/DiscordBot.java
@@ -22,53 +22,53 @@ import java.awt.*;
 
 public class DiscordBot extends ListenerAdapter {
 
-     public static JDA jda;
-     public static String prefix = "htb!";
+    public static JDA jda;
+    public static String prefix = "htb!";
 
-     public static void start() {
-          try {
-               jda = JDABuilder.createDefault("OTMwNTg3OTI5MzIyMDk0NTkz.Yd4Diw.E04gLo9MW7UaZT-23zjH94r5aHs").build();
+    public static void start() {
+        try {
+            jda = JDABuilder.createDefault("OTMwNTg3OTI5MzIyMDk0NTkz.Yd4Diw.E04gLo9MW7UaZT-23zjH94r5aHs").build();
 
-               jda.getPresence().setActivity(Activity.playing("HypeTime.eu"));
+            jda.getPresence().setActivity(Activity.playing("HypeTime.eu"));
 
-               registerListener(new Commands());
-               registerListener(new OwnChannel());
-               registerListener(new Suggestion());
-               registerListener(new ChatFilter());
-               registerListener(new TicketListener());
-               registerListener(new JoinListener());
-               registerListener(new DiscordBot());
-               registerListener(new RulesListener());
-               registerListener(new KeyGenerationMessageListener());
-               registerListener(new BetaSystemCommands());
-               registerListener(new VerifySystemCommand());
-               registerListener(new top5Command());
+            registerListener(new Commands());
+            registerListener(new OwnChannel());
+            registerListener(new Suggestion());
+            registerListener(new ChatFilter());
+            registerListener(new TicketListener());
+            registerListener(new JoinListener());
+            registerListener(new DiscordBot());
+            registerListener(new RulesListener());
+            registerListener(new KeyGenerationMessageListener());
+            registerListener(new BetaSystemCommands());
+            registerListener(new VerifySystemCommand());
+            registerListener(new top5Command());
 
-               jda.getPresence().setActivity(Activity.playing("HypeTime v3"));
-               jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
+            jda.getPresence().setActivity(Activity.playing("HypeTime v3"));
+            jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
 
-               Logger.info("Erfolgreich gestartet");
-          } catch (LoginException exception) {
-               Logger.error("Discord bot konnte nicht gestartet werden." +
+            Logger.info("Erfolgreich gestartet");
+        } catch (LoginException exception) {
+            Logger.error("Discord bot konnte nicht gestartet werden." +
                     " Es ist ein Fehler aufgetreten." +
                     " Fehlermeldung: " + exception.getMessage());
-          }
-     }
+        }
+    }
 
-     public static void registerListener(Object object) {
-          jda.addEventListener(object);
-          Logger.info("Register Listener " + object.getClass().getSimpleName());
-     }
+    public static void registerListener(Object object) {
+        jda.addEventListener(object);
+        Logger.info("Register Listener " + object.getClass().getSimpleName());
+    }
 
-     @Override
-     public void onReady(@NotNull ReadyEvent event) {
-          EmbedBuilder builder = new EmbedBuilder();
-          builder.setColor(Color.GREEN);
-          builder.setTitle("ProxySystem");
-          builder.addField("Status:", "Online", true);
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setColor(Color.GREEN);
+        builder.setTitle("HypeTime Beta Proxy");
+        builder.addField("Status:", "Online", true);
 
-          DiscordBot.jda.getGuildById("693086914491842570").getTextChannelById("786243505319051284").sendMessage(builder.build()).queue();
-     }
+        DiscordBot.jda.getGuildById("693086914491842570").getTextChannelById("786243505319051284").sendMessage(builder.build()).queue();
+    }
 
 
 }
